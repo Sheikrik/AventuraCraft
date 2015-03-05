ba5454f31e928f55abb35055380a989678ccd94e
Mauricio Del Castillo Morales <sheikrik@gmail.com>
2015-03-04 18:46:21 -0600

    Añadiendo contenido nuevo
    Creación de contacto.html, tienda.html y algunos detalles graficos.

--- js/funciones.js
+++ js/funciones.js
@@ -8,3 +8,20 @@ $(function() {
 		timeout: 6500
 	});
 });
+
+// Evento que muestra y esconde la informacion acerca de los servicios
+
+$(".staff figure").each(function(i){
+	$("#staff_" + i).click(function(){
+		$("#background_" + i).fadeIn(1000);
+	});
+});
+$(".icon-close").click(function(){
+	$(".presentacion_background").fadeOut(1000);
+});
+
+// Evento que muestra y esconde los submenus de "servidor"
+
+$(".servidor").click(function(){
+	$("#menu_servidor").slideToggle(1000);
+});
